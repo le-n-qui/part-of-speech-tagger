de3113073c57e246dc5040870f53dfa9158ae631
Andy Le <andy.quile@gmail.com>
2022-12-10 23:20:00 -0800

    Create main method

--- pos_tagger.py
+++ pos_tagger.py
@@ -11,21 +11,8 @@ import pprint, time
 # download the Brown corpus
 from nltk.corpus import brown
 
-# download the universal tagset from nltk
-nltk.download("universal_tagset")
 
-# retrieve word and its tag in brown corpus
-data = list(brown.tagged_sents(tagset="universal"))
-
-# create train and test set
-train, test = train_test_split(data, train_size=0.8, test_size=0.2, random_state=42)
-
-# create train tagged words
-train_tagged_words = [pair for sent in train for pair in sent]
-# create test tagged words
-test_tagged_words = [pair for sent in test for pair in sent]
-
-def count_word_and_tag(word, tag, train_data=train_tagged_words):
+def count_word_and_tag(word, tag, train_data):
     """This method returns a tuple
        containing the count of the provided
        word with the given tag and the
@@ -46,7 +33,7 @@ def count_word_and_tag(word, tag, train_data=train_tagged_words):
 
     return (word_as_tag_count, tag_count)
 
-def count_tag2_given_tag1(t2, t1, train_data=train_tagged_words):
+def count_tag2_given_tag1(t2, t1, train_data):
     """This method returns a tuple
        containing the count of tag 2
        given tag 1 and the count of 
@@ -61,3 +48,39 @@ def count_tag2_given_tag1(t2, t1, train_data=train_tagged_words):
             count_t2_given_t1 += 1
 
     return (count_t2_given_t1, t1_count)
+
+def main():
+    # download the universal tagset from nltk
+    nltk.download("universal_tagset")
+
+    # retrieve word and its tag in brown corpus
+    data = list(brown.tagged_sents(tagset="universal"))
+
+    # create train and test set
+    train, test = train_test_split(data, train_size=0.8, test_size=0.2, random_state=42)
+
+    # create train tagged words
+    train_tagged_words = [pair for sent in train for pair in sent]
+
+    # create test tagged words
+    test_tagged_words = [pair for sent in test for pair in sent]
+
+    # set of tags
+    tags = {tag for word, tag in train_tagged_words}
+    # set of words (vocabulary)
+    vocab = {word for word, tag in train_tagged_words}
+
+    # create a transition matrix of tags
+    # matrix(i, j) represents the Probability(jth tag after ith tag)
+    tags_matrix = np.zeros((len(tags), len(tags)), dtype="float32")
+    for i, t1 in enumerate(list(tags)):
+        for j, t2 in enumerate(list(tags)):
+            t2_given_t1_count, t1_count = count_tag2_given_tag1(t2, t1, train_tagged_words)
+            tags_matrix[i,j] = t2_given_t1_count/t1_count
+
+    # convert matrix into dataframe
+    tags_df = pd.DataFrame(tags_matrix, columns=list(tags), index=list(tags))
+
+
+if __name__ == "__main__":
+    main()
